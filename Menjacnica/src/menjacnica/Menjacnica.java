ac53bd54a6ead26e4e2a12d84aab4ea57fe03239
miljan996 <miljan.ignjatovic.96@gmail.com>
2017-03-28 20:14:46 +0200

    Kreirana klasa Menjacnica koja implementira interfejs MenjacnicaInterfejs

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajDnevniKurs(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiDnevniKurs(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Valuta vratiKurs(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
