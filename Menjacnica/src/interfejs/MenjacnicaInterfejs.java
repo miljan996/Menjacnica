8abbcff0a4461c141a89fac825c1921c7db0b4c9
miljan996 <miljan.ignjatovic.96@gmail.com>
2017-03-28 20:40:00 +0200

    Implementiran interfejs MenjacnicaInterfejs u klasu Menjacnica uz izmene samog interfejsa

--- Menjacnica/src/interfejs/MenjacnicaInterfejs.java
+++ Menjacnica/src/interfejs/MenjacnicaInterfejs.java
@@ -2,14 +2,15 @@ package interfejs;
 
 import java.util.GregorianCalendar;
 
+import menjacnica.DnevniKurs;
 import menjacnica.Valuta;
 
 public interface MenjacnicaInterfejs {
 
-	public void dodajDnevniKurs(GregorianCalendar datum);
+	public void dodajDnevniKurs(GregorianCalendar datum, Valuta val, double prodajni, double kupovni, double srednji);
 	
 	public void obrisiDnevniKurs(GregorianCalendar datum);
 	
-	public Valuta vratiKurs(GregorianCalendar datum);
+	public DnevniKurs vratiKurs(GregorianCalendar datum);
 	
 }
--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,26 +1,50 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 import interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
 
-	@Override
-	public void dodajDnevniKurs(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+	private LinkedList<DnevniKurs> dnevniKursevi = new LinkedList<DnevniKurs>();
 
+	@Override
+	public void dodajDnevniKurs(GregorianCalendar datum, Valuta val, double prodajni, double kupovni, double srednji) {
+		DnevniKurs temp = new DnevniKurs();
+
+		temp.setDatum(datum);
+		temp.setValuta(val);
+		temp.setProdajni(prodajni);
+		temp.setKupovni(kupovni);
+		temp.setSrednji(srednji);
+
+		dnevniKursevi.add(temp);
 	}
 
 	@Override
 	public void obrisiDnevniKurs(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < dnevniKursevi.size(); i++) {
+			DnevniKurs temp = dnevniKursevi.get(i);
 
+			if (temp.getDatum().equals(datum)) {
+				dnevniKursevi.remove(i);
+				return;
+			}
+		}
 	}
 
 	@Override
-	public Valuta vratiKurs(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+	public DnevniKurs vratiKurs(GregorianCalendar datum) {
+		for (int i = 0; i < dnevniKursevi.size(); i++) {
+			DnevniKurs temp = dnevniKursevi.get(i);
+
+			if (temp.getDatum().equals(datum)) {
+				return dnevniKursevi.get(i);
+			}
+		}
+
 		return null;
 	}
 
