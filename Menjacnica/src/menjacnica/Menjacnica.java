8a30aeb764564dc54a20b7d07a47dca7b5a10e92
miljan996 <miljan.ignjatovic.96@gmail.com>
2017-03-28 20:50:13 +0200

    Implementirana metoda dodajDnevniKurs

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,15 +1,25 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
 
+	private LinkedList<DnevniKurs> dnevniKursevi = new LinkedList<DnevniKurs>();
+	
 	@Override
 	public void dodajDnevniKurs(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
+		DnevniKurs temp = new DnevniKurs();
+		
+		temp.setDatum(datum);
+		temp.setKupovni(124);
+		temp.setProdajni(123);
+		temp.setSrednji(122);
+		temp.setValuta(null);
+		
+		dnevniKursevi.add(temp);
 	}
 
 	@Override
