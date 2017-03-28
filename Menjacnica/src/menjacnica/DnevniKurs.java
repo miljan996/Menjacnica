f814131744a109ba676607d02b57b72dd837ab22
miljan996 <miljan.ignjatovic.96@gmail.com>
2017-03-28 18:50:36 +0200

    Dodate metode hashCode, equals i toString u klase DnevniKurs i Valuta

--- Menjacnica/src/menjacnica/DnevniKurs.java
+++ Menjacnica/src/menjacnica/DnevniKurs.java
@@ -1,5 +1,103 @@
 package menjacnica;
 
+import java.util.GregorianCalendar;
+
 public class DnevniKurs {
+
+	private Valuta valuta;
+	private GregorianCalendar datum;
+	private double prodajni;
+	private double kupovni;
+	private double srednji;
+
+	public Valuta getValuta() {
+		return valuta;
+	}
+
+	public void setValuta(Valuta valuta) {
+		this.valuta = valuta;
+	}
+
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+
+	public double getProdajni() {
+		return prodajni;
+	}
+
+	public void setProdajni(double prodajni) {
+		this.prodajni = prodajni;
+	}
+
+	public double getKupovni() {
+		return kupovni;
+	}
+
+	public void setKupovni(double kupovni) {
+		this.kupovni = kupovni;
+	}
+
+	public double getSrednji() {
+		return srednji;
+	}
+
+	public void setSrednji(double srednji) {
+		this.srednji = srednji;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(prodajni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(srednji);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((valuta == null) ? 0 : valuta.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DnevniKurs other = (DnevniKurs) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (Double.doubleToLongBits(kupovni) != Double.doubleToLongBits(other.kupovni))
+			return false;
+		if (Double.doubleToLongBits(prodajni) != Double.doubleToLongBits(other.prodajni))
+			return false;
+		if (Double.doubleToLongBits(srednji) != Double.doubleToLongBits(other.srednji))
+			return false;
+		if (valuta == null) {
+			if (other.valuta != null)
+				return false;
+		} else if (!valuta.equals(other.valuta))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "DnevniKurs [valuta=" + valuta + ", datum=" + datum + ", prodajni=" + prodajni + ", kupovni=" + kupovni
+				+ ", srednji=" + srednji + "]";
+	}
 
 }
--- Menjacnica/src/menjacnica/Valuta.java
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -2,4 +2,59 @@ package menjacnica;
 
 public class Valuta {
 
+	private String valuta;
+	private String skracenica;
+
+	public String getValuta() {
+		return valuta;
+	}
+
+	public void setValuta(String valuta) {
+		this.valuta = valuta;
+	}
+
+	public String getSkracenica() {
+		return skracenica;
+	}
+
+	public void setSkracenica(String skracenica) {
+		this.skracenica = skracenica;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((skracenica == null) ? 0 : skracenica.hashCode());
+		result = prime * result + ((valuta == null) ? 0 : valuta.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (skracenica == null) {
+			if (other.skracenica != null)
+				return false;
+		} else if (!skracenica.equals(other.skracenica))
+			return false;
+		if (valuta == null) {
+			if (other.valuta != null)
+				return false;
+		} else if (!valuta.equals(other.valuta))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Valuta [valuta=" + valuta + ", skracenica=" + skracenica + "]";
+	}
+
 }
